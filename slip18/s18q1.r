b17a83f074fd1600487157c66ba26e7586b0fcdf
aurix4632 <aurix4632@gmail.com>
2024-10-03 19:10:02 +0530

    Add files via upload

---
+++ slip18/s18q1.r
@@ -0,0 +1,7 @@
+vector <- c(12, 5, 8, 23, 2, 16)
+max_value <- max(vector)
+min_value <- min(vector)
+cat("Given vector:", vector, "\n")
+cat("Maximum value:", max_value, "\n")
+cat("Minimum value:", min_value, "\n")
+
