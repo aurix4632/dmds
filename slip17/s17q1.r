5a2c24c51e8ca2d6865dcc4a67304697d7bda514
aurix4632 <aurix4632@gmail.com>
2024-10-21 15:41:56 +0530

    Update s17q1.r

--- slip17/s17q1.r
+++ slip17/s17q1.r
@@ -1,23 +1,11 @@
-# Function to calculate Fibonacci numbers
-fibonacci_numbers <- function(n) {
-  # Initialize the first two Fibonacci numbers
-  fib <- numeric(n)
-  fib[1] <- 0
-  fib[2] <- 1
-  
-  # Calculate Fibonacci numbers
-  for (i in 3:n) {
-    fib[i] <- fib[i - 1] + fib[i - 2]
-  }
-  
-  return(fib)
+fibonacci<-function(n){
+fib<-numeric(n)
+fib[1]<-0
+fib[2]<-1
+for(i in 3:n){
+fib[i]<-fib[i-1]+fib[i-2]
 }
-
-# Get the first 20 Fibonacci numbers
-first_20_fibonacci <- fibonacci_numbers(20)
-
-# Display the result
-cat("The first 20 Fibonacci numbers are:\n")
-print(first_20_fibonacci)
-
-
+return(fib)
+}
+first_20_fib<-fibonacci(20)
+print(first_20_fib)
